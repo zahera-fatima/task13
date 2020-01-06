a8d4fac0d2e729519c0f4c0019a33d65d728750e
zahera-fatima <55752747+zahera-fatima@users.noreply.github.com>
2020-01-07 02:26:17 +0500

    Add files via upload

---
+++ fib.py
@@ -0,0 +1,17 @@
+import argparse
+
+def fib(n):
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a+b
+    return a
+
+def Main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("num", help="the fibonacci no you wish to calculate:",type=int)
+    args = parser.parse_args()
+    result = fib(args.num)
+    print("the"+str(args.num)+"th fib no is "+str(result))
+
+if __name__ == "__main__":
+    Main() 
